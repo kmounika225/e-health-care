9780d217fbd563963065a81495f860d5aca22770
Sai Pavan Maram <saipavanmaram@Sais-MacBook-Pro.local>
2021-11-18 23:34:55 -0600

    Init

---
+++ src/main/java/com/healthcare/EHealthCareApplication.java
@@ -0,0 +1,14 @@
+package com.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHealthCareApplication.class, args);
+		System.out.println("djfd");
+	}
+
+}
